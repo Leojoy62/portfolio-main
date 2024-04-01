709464b2304031e14cb19e5d0aa988db5e1ed3c5
Leojoy62 <leojoy62@gmail.com>
2024-04-02 03:43:48 +0600

    font problem solved

--- src/ProtfolioContainer/Home/NavBar.jsx
+++ src/ProtfolioContainer/Home/NavBar.jsx
@@ -4,27 +4,6 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <div className="px-[4%]">
-      {/* <div className="pt-10 px-[5%]">
-        <ul className="text-white flex gap-16 justify-between items-center text-2xl font-semibold">
-          <li className="text-5xl font-bold">
-            LEO<span className="text-orange-600">N</span>ARD.
-          </li>
-          <ul className="text-white flex gap-16 justify-center items-center text-2xl font-semibold">
-            <li className="style-nav">
-              <a href="#home">Home</a>
-            </li>
-            <li className="style-nav">
-              <a href="#about">AboutMe</a>
-            </li>
-            <li className="style-nav">
-              <a href="#resume">Resume</a>
-            </li>
-            <li className="style-nav">
-              <a href="#contact">ContactMe</a>
-            </li>
-          </ul>
-        </ul>
-      </div> */}
       <div className="navbar text-white">
         <div className="navbar-start">
           <div className="dropdown">
@@ -69,7 +48,7 @@ const NavBar = () => {
         </div>
 
         <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-2xl font-semibold">
+          <ul className="menu menu-horizontal px-1 text-xl font-semibold">
             <li className="style-nav">
               <a href="#home">Home</a>
             </li>
